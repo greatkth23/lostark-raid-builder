b784ecf549b00c6a2ad56744959b9520b8fe6378
김태환 <greatkth23@gmail.com>
2026-07-21 10:45:31 +0900

    Update favicon and resize gold icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ export const metadata: Metadata = {
   title: "로스트아크 공격대 자동구성",
   description: "멤버와 레이드 진행 현황을 함께 관리하는 로스트아크 공격대 자동구성 도구",
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: "/lostark_class_aero_favicon.png",
+    shortcut: "/lostark_class_aero_favicon.png",
+    apple: "/lostark_class_aero_favicon.png",
   },
 };
 
